63b7763b4bad9d5e499222d84c887f55f2366611
atrantan <atrantan@gmail.com>
2023-09-28 17:08:56 +0200

    fix qt interoperability

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+//  Copyright (c) 2023 Antoine TRAN TAN
 //  Copyright (c) 2020 Victor BOUVARD
 //  Copyright (c) 2011 Philipp Wagner
 //
@@ -12,32 +13,53 @@
 #include <fstream>
 #include <sstream>
 
+#include "mainwindow.h"
+#include <QApplication>
+#include <thread>
+
 using namespace cv;
 using namespace cv::face;
 using namespace std;
 
-static void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator = ';') {
+static void read_csv(const string &filename, vector<Mat> &images, vector<int> &labels, char separator = ';')
+{
     std::ifstream file(filename.c_str(), ifstream::in);
-    if (!file) {
+    if (!file)
+    {
         string error_message = "No valid input file was given, please check the given filename.";
         CV_Error(Error::StsBadArg, error_message);
     }
     string line, path, classlabel;
-    while (getline(file, line)) {
+    while (getline(file, line))
+    {
         stringstream liness(line);
         getline(liness, path, separator);
         getline(liness, classlabel);
-        if(!path.empty() && !classlabel.empty()) {
+        if (!path.empty() && !classlabel.empty())
+        {
             images.push_back(imread(path, 0));
             labels.push_back(atoi(classlabel.c_str()));
         }
     }
 }
 
-int main(int argc, const char *argv[]) {
+int main(int argc, char *argv[])
+{
+    std::thread t(
+        [&argc, &argv]()
+        {
+            QApplication a(argc, argv);
+            MainWindow w;
+
+            w.show();
+
+            return QApplication::exec();
+        });
+
     // Check for valid command line arguments, print usage
     // if no arguments were given.
-    if (argc != 4) {
+    if (argc != 4)
+    {
         cout << "usage: " << argv[0] << " </path/to/haar_cascade> </path/to/csv.ext> </path/to/device id>" << endl;
         cout << "\t </path/to/haar_cascade> -- Path to the Haar Cascade for face detection." << endl;
         cout << "\t </path/to/csv.ext> -- Path to the CSV file with the face database." << endl;
@@ -52,9 +74,12 @@ int main(int argc, const char *argv[]) {
     vector<Mat> images;
     vector<int> labels;
     // Read in the data (fails if no valid input filename is given, but you'll get an error message):
-    try {
+    try
+    {
         read_csv(fn_csv, images, labels);
-    } catch (const cv::Exception& e) {
+    }
+    catch (const cv::Exception &e)
+    {
         cerr << "Error opening file \"" << fn_csv << "\". Reason: " << e.msg << endl;
         // nothing more we can do
         exit(1);
@@ -77,13 +102,15 @@ int main(int argc, const char *argv[]) {
     // Get a handle to the Video device:
     VideoCapture cap(deviceId);
     // Check if we can use this device at all:
-    if(!cap.isOpened()) {
+    if (!cap.isOpened())
+    {
         cerr << "Capture Device ID " << deviceId << "cannot be opened." << endl;
         return -1;
     }
     // Holds the current frame from the Video device:
     Mat frame;
-    for(;;) {
+    for (;;)
+    {
         cap >> frame;
         // Clone the current frame:
         Mat original = frame.clone();
@@ -91,12 +118,13 @@ int main(int argc, const char *argv[]) {
         Mat gray;
         cvtColor(original, gray, COLOR_BGR2GRAY);
         // Find the faces in the frame:
-        vector< Rect_<int> > faces;
+        vector<Rect_<int>> faces;
         haar_cascade.detectMultiScale(gray, faces);
         // At this point you have the position of the faces in
         // faces. Now we'll get the faces, make a prediction and
         // annotate it in the video. Cool or what?
-        for(size_t i = 0; i < faces.size(); i++) {
+        for (size_t i = 0; i < faces.size(); i++)
+        {
             // Process face by face:
             Rect face_i = faces[i];
             // Crop the face from the image. So simple with OpenCV C++:
@@ -117,28 +145,27 @@ int main(int argc, const char *argv[]) {
             int prediction = model->predict(face_resized);
             // And finally write all we've found out to the original image!
             // First of all draw a green rectangle around the detected face:
-            rectangle(original, face_i, Scalar(0, 255,0), 1);
+            rectangle(original, face_i, Scalar(0, 255, 0), 1);
             // Create the text we will annotate the box with:
             string box_text;
-            if( prediction == 1)
+            if (prediction == 1)
                 box_text = format("Bruce");
-            else if( prediction == 2 )
+            else if (prediction == 2)
                 box_text = format("Victor");
             // Calculate the position for annotated text (make sure we don't
             // put illegal values in there):
             int pos_x = std::max(face_i.tl().x - 10, 0);
             int pos_y = std::max(face_i.tl().y - 10, 0);
             // And now put it into the image:
-            putText(original, box_text, Point(pos_x, pos_y), FONT_HERSHEY_PLAIN, 1.0, Scalar(0,255,0), 2);
+            putText(original, box_text, Point(pos_x, pos_y), FONT_HERSHEY_PLAIN, 1.0, Scalar(0, 255, 0), 2);
         }
         // Show the result:
         imshow("face_recognizer", original);
         // And display it:
-        char key = (char) waitKey(20);
+        char key = (char)waitKey(20);
         // Exit this loop on escape:
-        if(key == 27)
+        if (key == 27)
             break;
     }
     return 0;
 }
-
